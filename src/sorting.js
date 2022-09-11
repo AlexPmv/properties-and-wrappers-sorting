96406a5ccf33cbd30b680399afe28763a4adab23
“Alex <likeitsweet@mail.ru>
2022-09-11 15:04:15 +0300

    init commit (task in word)

---
+++ src/sorting.js
@@ -0,0 +1,29 @@
+export default function orderByProps(obj, orderArray) {
+  const primaryProps = [];
+  const sideProps = [];
+
+  for (const property in obj) {
+    if (orderArray.includes(property)) {
+      primaryProps[orderArray.findIndex((item) => item === property)] = {
+        key: property, value: obj[property],
+      };
+    } else {
+      sideProps.push({ key: property, value: obj[property] });
+    }
+  }
+
+  sideProps.sort((a, b) => {
+    if (a.key < b.key) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return primaryProps.concat(sideProps);
+}
+
+// const obj1 = {
+//   name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+// };
+
+// console.log(orderByProps(obj1, ['name', 'level']));
