a501d4be4a33a63325648d354b99b0bfac3a3421
Alex Palmov <likeitsweet@mail.ru>
2022-09-11 23:02:57 +0300

    result commit

--- src/character.js
+++
@@ -1,9 +0,0 @@
-// export default class Character {
-//   constructor(name) {
-//     this.name = name;
-//     this.health = 100;
-//     this.level = 1;
-//     this.attack = 80;
-//     this.defence = 40;
-//   }
-// }
--- src/sorting.js
+++ src/sorting.js
@@ -21,9 +21,3 @@ export default function orderByProps(obj, orderArray) {
 
   return primaryProps.concat(sideProps);
 }
-
-// const obj1 = {
-//   name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-// };
-
-// console.log(orderByProps(obj1, ['name', 'level']));
